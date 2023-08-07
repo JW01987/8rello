058a3773a3b4221d111d662e96529526f0d30f5f
jegwlee <jegwlee@gmail.com>
2023-08-07 20:53:07 +0900

    Add: bcrypt로 비밀번호 암호화

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
 
 // 회원가입, 사용자 정보 수정
 @Injectable()
@@ -14,17 +15,26 @@ export class UsersService {
     if (existingUser) {
       throw new HttpException('이미 존재하는 사용자입니다.', HttpStatus.CONFLICT);
     }
-    await this.userRepository.save(user);
-    return { message: '회원가입이 성공적으로 완료되었습니다.' };
+    const encryptedPassword = bcrypt.hashSync(user.password, 10);
+
+    try {
+      await this.userRepository.save({ ...user, password: encryptedPassword });
+      return { message: '회원가입이 성공적으로 완료되었습니다.' };
+    } catch (error) {
+      throw new HttpException('서버 에러', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async updateUser(id: number, user: UpdateUserDto): Promise<{ message: string }> {
     const existingUser: User = await this.userRepository.findOne({ where: { id } });
-
     if (!existingUser) throw new NotFoundException('id와 일치하는 유저가 없습니다.');
     existingUser.username = user.username;
-    this.userRepository.save(existingUser);
 
-    return { message: '닉네임이 수정되었습니다.' };
+    try {
+      await this.userRepository.save(existingUser);
+      return { message: '닉네임이 수정되었습니다.' };
+    } catch (error) {
+      throw new HttpException('서버 에러', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
